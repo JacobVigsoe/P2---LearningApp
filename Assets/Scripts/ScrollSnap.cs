c83d1d4d8621dc3d8433995aefc72aca49f3b1d1
jacob vigso <eh66xs@student.aau.dk>
2024-04-10 16:14:37 +0200

    Graph thing

---
+++ Assets/Scripts/ScrollSnap.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+
+public class ScrollSnap : MonoBehaviour
+{
+    public RectTransform panel; // To hold the scroll panel
+    public RectTransform center; // To hold the center point for comparison
+    public TMP_Text[] texts; // Array of TextMeshPro text elements within the scroll view
+    public float[] distance; // Distance of each text element from the center vertically
+    public bool dragging = false; // Flag to check if the user is dragging
+    public float padding = 80f; // Padding applied to the viewport's Vertical Layout Group
+    public float snapSpeed = 10f; // Snapping speed
+
+    void Start()
+    {
+        int textLength = texts.Length;
+        distance = new float[textLength];
+    }
+
+    void Update()
+    {
+      
+        
+            // Calculate distances of each text element from the center vertically
+            for (int i = 0; i < texts.Length; i++)
+            {
+                distance[i] = Mathf.Abs(center.position.y - texts[i].transform.position.y);
+            }
+        
+      
+
+        // Find the closest text element
+        float minDistance = Mathf.Min(distance);
+        int minTextIndex = 0;
+        for (int i = 0; i < distance.Length; i++)
+        {
+            if (distance[i] == minDistance)
+            {
+                minTextIndex = i;
+                break;
+            }
+        }
+      
+        
+            // Snap to the closest text element with padding adjustment and snapping speed
+            LerpToText(minTextIndex, padding, snapSpeed);
+        
+        
+    }
+
+    void LerpToText(int textIndex, float padding, float speed)
+    {
+        // Calculate the target position including padding adjustment
+        float targetY = -texts[textIndex].rectTransform.anchoredPosition.y - padding;
+
+        // Lerp towards the target position with specified speed
+        float newY = Mathf.Lerp(panel.anchoredPosition.y, targetY, speed * Time.deltaTime);
+        Vector2 newPosition = new Vector2(panel.anchoredPosition.x, newY);
+        panel.anchoredPosition = newPosition;
+    }
+
+    public void StartDrag()
+    {
+        dragging = true;
+    }
+
+    public void EndDrag()
+    {
+        dragging = false;
+    }
+}
---
+++ Assets/Scripts/Stats/WindowGraph.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+using TMPro;
+
+public class WindowGraph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
+    private RectTransform dashTemplateX;
+    private RectTransform dashTemplateY;
+    private List<GameObject> gameObjectList;
+
+    [SerializeField] private int ShowLastListAmount = -1;
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent <RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        dashTemplateY = graphContainer.Find("dashTemplateY").GetComponent<RectTransform>();
+        gameObjectList = new List<GameObject>();
+
+
+        List<int> ValueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 12, 17, 25, 27, 37, 40, 36, 44, 61, 50, 40, 30, 20, 10, 23, 25, 29, 45, 12, 13, 19, 23, 30, 40, 55, 46, 67, 76, 78, 89, 90};
+
+        ShowGraph(ValueList, ShowLastListAmount, (int _i) => "Day "+(_i+1), (float _f) => "$" + Mathf.RoundToInt(_f));
+
+      /*  FunctionPeriodic.Create(() =>
+        {
+            ValueList.Clear();
+            for (int i = 0; i<15; i++)
+            {
+                ValueList.Add(UnityEngine.Random.Range(0, 500));
+            }
+            ShowGraph(ValueList, (int _i) => "Day " + (_i + 1), (float _f) => "$" + Mathf.RoundToInt(_f));
+        }, .5f); //trigger every .5 seconds */
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition) 
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer,false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+
+        return gameObject;
+    }
+
+    private void ShowGraph(List<int> valueList, int maxVisibleValueAmount = -1, Func<int, string> getAxisLabelX = null, Func<float, string> getAxisLabelY = null)
+    {
+
+        if(getAxisLabelX == null)
+        {
+            getAxisLabelX = delegate (int _i) { return _i.ToString(); };
+        }
+
+        if (getAxisLabelY == null)
+        {
+            getAxisLabelY = delegate (float _f) { return Mathf.RoundToInt(_f).ToString(); };
+        }
+
+        if (maxVisibleValueAmount <= 0)
+        {
+            maxVisibleValueAmount = valueList.Count;
+        }
+
+        foreach (GameObject gameObject in gameObjectList)
+        {
+            Destroy(gameObject);
+        }
+
+        gameObjectList.Clear();
+
+        float graphWidth = graphContainer.sizeDelta.x;
+        float graphHeight = graphContainer.sizeDelta.y;
+
+        float yMaximum = valueList[0];
+        float yMinimum = valueList[0];
+
+        for (int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++) //hvis vi har mindre end 5 values i vores data set så ville den returne error (valueList.Count - maxVisibleValueAmount vil gå i minus). Mathf.Max gør at den istedet returner 0 så vi ikke får en error.
+        {
+            int value = valueList[i];
+
+            if (value > yMaximum)
+            {
+                yMaximum = value;
+            }
+            if (value < yMinimum)
+            {
+                yMinimum = value;
+            }
+        }
+
+        float yDifference = yMaximum - yMinimum;
+        if (yDifference <= 0)
+        {
+            yDifference = 5f;
+        }
+        yMaximum = yMaximum + (yDifference * 0.2f);
+        yMinimum = yMinimum - (yDifference * 0.2f);
+
+        float xSize = graphWidth / (maxVisibleValueAmount + 1); // +1 så den ikke er helt oppe af højre side på grafen
+
+        int xIndex = 0;
+
+        GameObject lastCircleGameObject = null;
+        for (int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++) // hvis maxVisibleValueAmount er fx 5 vil den vise de sidste 5 værdier 
+        {
+            float xPosition = xSize + xIndex * xSize;
+            float yPosition = ((valueList[i] - yMinimum) / (yMaximum - yMinimum)) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            gameObjectList.Add(circleGameObject);
+            if(lastCircleGameObject != null )
+            {
+                GameObject dotConnectionGameObject = CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+                gameObjectList.Add(dotConnectionGameObject);
+            }
+            lastCircleGameObject = circleGameObject;
+
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition3D = new Vector3(xPosition, -7f, 0f);
+            labelX.GetComponent<TMP_Text>().text = getAxisLabelX(i);
+            gameObjectList.Add(labelX.gameObject);
+
+            RectTransform dashY = Instantiate(dashTemplateY);
+            dashY.SetParent(graphContainer);
+            dashY.gameObject.SetActive(true);
+            dashY.anchoredPosition3D = new Vector3(xPosition, -7f, 0f);
+            gameObjectList.Add(dashY.gameObject);
+
+            xIndex++;
+        }
+
+        int seperatorCount = 10;
+        for (int i = 0;i <= seperatorCount;i++) 
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i * 1f / seperatorCount;
+            labelY.anchoredPosition3D = new Vector3(-7f, normalizedValue * graphHeight, 0f);
+            labelY.GetComponent<TMP_Text>().text = getAxisLabelY(yMinimum + (normalizedValue * (yMaximum - yMinimum)));
+            gameObjectList.Add(labelY.gameObject);
+
+            RectTransform dashX = Instantiate(dashTemplateX);
+            dashX.SetParent(graphContainer);
+            dashX.gameObject.SetActive(true);
+            dashX.anchoredPosition3D = new Vector3(-4, normalizedValue * graphHeight, 0f);
+            gameObjectList.Add(dashX.gameObject);
+        }
+    }
+
+    private GameObject CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof (Image));
+        gameObject.transform.SetParent(graphContainer,false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir  * distance * .5f; // Så den bliver positioned i midten af de 2 points (.5f)
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+        return gameObject;
+    }
+
+}
