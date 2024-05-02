dcb886e303deac184b7754c9f8d2f3677702bc87
Marcelkress <Marcel@kress.dk>
2024-05-02 11:11:16 +0200

    userdata

--- Assets/Goat scritps/SaveData.cs
+++ Assets/Goat scritps/SaveData.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 using System.IO;
 using System.Xml.Serialization;
 using System.Linq;
+using System;
 
 public class SaveData : MonoBehaviour
 {
     public TaskManager taskManager;
+    private string directoryPath;
+    private string userData;
+    private string userDataPath;
+
+    void Awake()
+    {
+        directoryPath = Application.dataPath + "/TaskInfo/";
+        userDataPath = Application.dataPath + "/UserData/";
+        userData = directoryPath + "UserData.json";
+    }
 
     public void SaveTasks(TaskInfo task)
     {
-        task.filePath = Application.dataPath + "/TaskInfo/" + task.taskName + ".json";
+        task.filePath = directoryPath + task.taskName + ".json";
 
         string json = JsonUtility.ToJson(task);
 
         File.WriteAllText(task.filePath, json);
     }
-
     public List<TaskInfo> LoadTasks()
     {
         List<TaskInfo> tasks = new List<TaskInfo>();
-
-        string directoryPath = Application.dataPath + "/TaskInfo/";
 
         if (Directory.Exists(directoryPath) && Directory.EnumerateFileSystemEntries(directoryPath).Any())
         {
@@ -37,10 +45,8 @@ public class SaveData : MonoBehaviour
         }
         return tasks;
     }
-
     public void DeleteTask(string name)
     {
-        string directoryPath = Application.dataPath + "/TaskInfo/";
         string filePath = directoryPath + name + ".json";
 
         if (Directory.Exists(directoryPath) && Directory.EnumerateFileSystemEntries(directoryPath).Any())
@@ -49,4 +55,26 @@ public class SaveData : MonoBehaviour
             File.Delete(filePath);
         }
     }
+
+    public void Save()
+    {
+        // Create a new directory for the save data
+        if (! Directory.Exists(userDataPath))
+            Directory.CreateDirectory(userDataPath);
+        
+        UserData data = new();
+
+        // Save the UserData class to a json file
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(userData, json);
+    }
+}
+
+[Serializable]
+public class UserData
+{
+    public int money;
+    public int currentCar;
+    public bool[] charactersUnlocked = new bool[6];
 }
